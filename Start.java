ee75e83e4bae091009e853b46bcd819c4016c366
sheikh-niloy <sheikh.niloy.info@gmail.com>
2024-03-04 03:43:21 +0600

    commit

---
+++ Start.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Start {
+
+    public static void main(String[] args) {
+       
+       
+        Scanner in = new Scanner(System.in);
+        String first1 = in.next();
+        String second1 = in.next();
+       
+        String first = first1.toLowerCase();
+        String second = second1.toLowerCase();
+       
+        if(first.compareTo(second) ==0){
+            System.out.println("0");
+        }
+        if(first.compareTo(second) > 0){
+            System.out.println("1");
+        }
+        if(first.compareTo(second) < 0){
+            System.out.println("-1");           
+        }
+    }
+}
